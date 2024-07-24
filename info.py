cd4b89f26999166bbde0fc1f1bfb78e4650e82ab
Su6666666 <86417238+Su6666666@users.noreply.github.com>
2024-07-24 21:53:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/3c65c2642f4339a8d3caa.jpg')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', 'https://telegra.ph/file/3c65c2642f4339a8d3caa.jpg https://telegra.ph/file/6b068b331c3faf05e8e41.jpg https://telegra.ph/file/54bfc48c12783c75ea692.jpg https://telegra.ph/file/9a96c17df5d784b2e3003.jpg https://telegra.ph/file/3142e1bb8f0d7948a853e.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/b69af2db776e4e85d21ec.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/How_To_Watch_SG_Videos/4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")

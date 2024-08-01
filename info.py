f0bab246d741cb59c748e792ee67ba7c4db74abd
Su6666666 <86417238+Su6666666@users.noreply.github.com>
2024-08-01 07:56:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/SG_Movies_Request')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/SG_Movie_Request')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/SGBACKUP')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Watch_SG_Videos/4')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'SG_Movies_Request') # Support Chat Link Without https:// or @

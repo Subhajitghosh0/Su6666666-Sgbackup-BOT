8d12d646fff7159af5fdd216db718c07af06b5ad
Su6666666 <86417238+Su6666666@users.noreply.github.com>
2024-08-01 07:57:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,7 +74,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/SG_Movie_Request')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/SGBACKUP')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Watch_SG_Videos/4')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'SG_Movies_Request') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'SG_Movie_Request') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))

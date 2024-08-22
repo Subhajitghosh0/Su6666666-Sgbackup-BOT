6f8c07e1e6ccef863fea18d8930428378ff98e4c
𝐒𝐮𝐛𝐡𝐚𝐣𝐢𝐭 𝐆𝐡𝐨𝐬𝐡 <86417238+Su6666666@users.noreply.github.com>
2024-08-22 09:06:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,10 +96,10 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'ziplinker.net')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '49be579e7f37d56e8f6987ba486b0aff1d0d1f77')
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'yamlinks.com')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '58e579aa6fe2c1cc91a7c2f1e79b57e9ab380c6a')
 # if verify second shortner is True then fill below url and api
 VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', '')
 VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
